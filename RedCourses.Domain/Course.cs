533bb0b2eb7e1a0406c1f834f1c9bfbd8a9a235f
juanonsoftware <huanhvhd@gmail.com>
2015-01-19 17:59:42 +0700

    Organize project structure

---
+++ RedCourses.Domain/Course.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RedCourses.Domain
+{
+    public class Course
+    {
+        private readonly IList<Video> _videos = new List<Video>();
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public IReadOnlyCollection<Video> Videos
+        {
+            get { return new ReadOnlyCollection<Video>(_videos); }
+        }
+
+        public void AddVideo(Video video)
+        {
+            if (_videos.Contains(video) == false)
+            {
+                _videos.Add(video);
+            }
+        }
+
+        public void RemoveVideo(Video video)
+        {
+            if (_videos.Contains(video))
+            {
+                _videos.Remove(video);
+            }
+        }
+    }
+}
---
+++ RedCourses.Domain/Video.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RedCourses.Domain
+{
+    public class Video
+    {
+        public Guid Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+    }
+}
