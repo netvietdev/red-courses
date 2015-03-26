dd04fe7b98ee7267eea8d92abb3f759865005249
juanonsoftware <huanhvhd@gmail.com>
2015-03-26 23:14:30 +0700

    Starting impl RavenDB

---
+++ RedCourses.Data.RavenImpl/Mappers/VideoMapFactory.cs
@@ -0,0 +1,10 @@
+﻿using Mapping.AutoMapper;
+using RedCourses.Data.RavenImpl.Documents;
+using RedCourses.Domain;
+
+namespace RedCourses.Data.RavenImpl.Mappers
+{
+    public class VideoMapFactory : AutoMapperFactory<Video, VideoDocument>
+    {
+    }
+}
