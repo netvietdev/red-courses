8ebb1fbc876d19d587ab9780383ce82b985fa655
juanonsoftware <huanhvhd@gmail.com>
2015-03-27 09:44:25 +0700

    Data

---
+++ RedCourses.Data/CoursesContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Entity;
+using RedCourses.Domain;
+
+namespace RedCourses.Data
+{
+    public class CoursesContext : DbContext 
+    {
+        public DbSet<Course> Courses { get; set; }
+    }
+}
